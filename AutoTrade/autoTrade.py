2684c5ff2c3d6dc6627a2b6508a44c9b462b8cd3
khj8619 <ilmare8619@naver.com>
2021-10-31 23:55:52 +0900

    commit success

--- AutoTrade/autoTrade.py
+++ AutoTrade/autoTrade.py
@@ -1,7 +1,10 @@
 import time
 import pyupbit
 import datetime
+import schedule
 import numpy as np
+import time
+from fbprophet import Prophet
 
 access = "your-access"
 secret = "your-secret"
@@ -9,6 +12,8 @@ ticker = "KRW-BTC" # 거래 coin
 buy_cur = "KRW" # 매수 화폐
 sel_cur = "BTC" # 매도 화폐
 count_val = 7 #7일간 최적 k value 추출
+predicted_close_price = 0
+opt_k_val = 0
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -58,19 +63,44 @@ def get_ror(k):
 
 def get_opt_k(k): 
     list_k = [0 for i in range(9)]
-    j = 0        
+    j = 0    
+    global opt_k_val   
     for k in np.arange(0.1, 1.0, 0.1):
         list_k[j] = get_ror(k)
         j = j+1
     k_max = max(list_k)
-    opt_k = (list_k.index(k_max) + 1)/10
-    return opt_k
+    opt_k_val = (list_k.index(k_max) + 1)/10
 
+def predict_price(ticker):
+    """Prophet으로 당일 종가 가격 예측"""
+    global predicted_close_price
+    df = pyupbit.get_ohlcv(ticker, interval="minute60")
+    df = df.reset_index()
+    df['ds'] = df['index']
+    df['y'] = df['close']
+    data = df[['ds','y']]
+    model = Prophet()
+    model.fit(data)
+    future = model.make_future_dataframe(periods=24, freq='H')
+    forecast = model.predict(future)
+    closeDf = forecast[forecast['ds'] == forecast.iloc[-1]['ds'].replace(hour=9)]
+    if len(closeDf) == 0:
+        closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=9)]
+    closeValue = closeDf['yhat'].values[0]
+    predicted_close_price = closeValue
+predict_price(ticker)
+schedule.every().hour.do(lambda: predict_price(ticker))
+schedule.every().hour.do(lambda: get_opt_k(1))
+
+def now_time():
+    now = time.localtime()    
+    print("TIME =======> %04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
+    
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
-opt_k_val = get_opt_k(1)
-print("autotrade start")
-print(opt_k_val)
+
+now_time()
+print(">> autotrade start !!")
 
 # 자동매매 시작
 while True:
@@ -78,19 +108,30 @@ while True:
         now = datetime.datetime.now()
         start_time = get_start_time(ticker)
         end_time = start_time + datetime.timedelta(days=1) #09:00 + 1일
+        
+        #print("opt_k_val => " + str(opt_k_val))
+        schedule.run_pending()
 
         # 09:00 < 현재 < #08:59:50
         if start_time < now < end_time - datetime.timedelta(seconds=10):  # 10초 전
             target_price = get_target_price(ticker, opt_k_val)
+            #print(target_price)
             ma15 = get_ma15(ticker)
             current_price = get_current_price(ticker)
             #if target_price < current_price and ma15 < current_price:
-            if target_price < current_price:
+            #if target_price < current_price:
+            if target_price < current_price and current_price < predicted_close_price:   
+                print(">> buy_market_order << ")
+                now_time()
                 krw = get_balance(buy_cur)
+                print("target_price :" + str(target_price) + "/ current_price :" + str(current_price) +"/ predicted_close_price :" + str(predicted_close_price)+"/ krw :" + str(krw))                 
                 if krw >= 10000 and krw <= 300000:
                     upbit.buy_market_order(ticker, krw*0.9995)
         else:
+            print(">> sell_market_order << ")
+            now_time()                        
             btc = get_balance(sel_cur)
+            print("target_price :" + str(target_price) + "/ current_price :" + str(current_price) +"/ predicted_close_price :" + str(predicted_close_price)+"/ btc :" + str(btc))                             
             if btc > 0.00008:
                 upbit.sell_market_order(ticker, btc*0.9995)
         time.sleep(1)
